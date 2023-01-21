986112edd586933049c5c8e6a7aeff35f0397c90
jasonbrianhall@gmail.com <jbhall@fbi.gov>
2023-01-21 10:49:20 -0500

    Revamped

--- mortgage.py
+++ mortgage.py
@@ -8,17 +8,20 @@ def mortgage_calculator(principal, rate, years):
     remaining_balance = principal
     interest_paid = 0
     payments = []
+    total_interest_paid=0
     for i in range(1, n+1):
         interest = remaining_balance * r
         principal_paid = monthly_payment - interest
         remaining_balance -= principal_paid
         interest_paid += interest
+        total_interest_paid=total_interest_paid+interest
         payments.append({
             'month': i, 
             'monthly_payment': round(monthly_payment, 2), 
             'principal_paid': round(principal_paid, 2),
             'interest_paid': round(interest, 2),
-            'remaining_balance': round(remaining_balance, 2)
+            'remaining_balance': round(remaining_balance, 2),
+            'total_interest_paid': round(total_interest_paid, 2) 
         })
     return payments
 
@@ -39,6 +42,7 @@ years = int(years)
 
 table = mortgage_calculator(principal, rate, years)
 
-print("{:<12}{:<12}{:<12}{:<12}{:<12}".format("Month", "Payment", "Principal", "Interest", "Remaining"))
+print("{:<12}{:<12}{:<12}{:<12}{:<12}{:<12}".format("Month", "Payment", "Principal", "Interest", "Remaining", "Total Interest"))
+totalinterestpaid=0
 for payment in table:
-    print("{:<12}{:<12}{:<12}{:<12}{:<12}".format(payment['month'], payment['monthly_payment'], payment['principal_paid'], payment['interest_paid'], payment['remaining_balance']))
+    print("{:<12}{:<12}{:<12}{:<12}{:<12}{:<12}".format(payment['month'], payment['monthly_payment'], payment['principal_paid'], payment['interest_paid'], payment['remaining_balance'], payment['total_interest_paid']))
